1df0227579fde71604c1ad9c0d3ddefddeb42aed
Andrew Kent <andrew@sprybts.com>
2018-05-07 12:03:41 +1000

    Reverting change for net_if test start

--- src/iperf_server_api.c
+++ src/iperf_server_api.c
@@ -442,7 +442,7 @@ sigend_handler(int sig)
 int
 iperf_run_server(struct iperf_test *test)
 {
-    int result, s, ls, streams_accepted;
+    int result, s, streams_accepted;
     fd_set read_set, write_set;
     struct iperf_stream *sp;
     struct timeval now;
@@ -528,7 +528,7 @@ iperf_run_server(struct iperf_test *test)
                             return -1;
 			}
 
-			// net_if_util(s, test->net_if_util);  /* Network i/f utilization - initialize */
+			net_if_util(s, test->net_if_util);  /* Network i/f utilization - initialize */
 			if (test->sender)
 			    FD_SET(s, &test->write_set);
 			else
@@ -571,11 +571,6 @@ iperf_run_server(struct iperf_test *test)
 			    if (test->listener > test->max_fd) test->max_fd = test->listener;
                         }
                     }
-
-		    /* Network i/f utilization - initialize using the most recently connected stream endpoint for the stats data */
-		    if (!is_closed(ls)) {
-			    net_if_util(ls, test->net_if_util);
-		    }
 
                     test->prot_listener = -1;
 		    if (iperf_set_send_state(test, TEST_START) != 0) {
