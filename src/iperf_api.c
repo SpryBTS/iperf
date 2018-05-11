21b2ee3ed6e7be36aab20bad784e6e68973c7c5d
Andrew Kent <andrew@sprybts.com>
2018-05-11 14:57:42 +1000

    integer sizing issues still

--- src/iperf_api.c
+++ src/iperf_api.c
@@ -2239,7 +2239,9 @@ iperf_print_results(struct iperf_test *test)
 	    iprintf(test, report_cpu, report_local, test->sender?report_sender:report_receiver, test->cpu_util[0], test->cpu_util[1], test->cpu_util[2], report_remote, test->sender?report_receiver:report_sender, test->remote_cpu_util[0], test->remote_cpu_util[1], test->remote_cpu_util[2]);
 
 	    if ((test->role == 'c') && (net_if_valid == 1)) {
+		printf("DEBUG: I'm about to print the stats");
 		iprintf(test, report_net_if, report_local, test->sender?report_sender:report_receiver, test->net_if_util[0], test->net_if_util[1], test->net_if_util[2], test->net_if_util[3], test->net_if_util[4], report_remote, test->sender?report_receiver:report_sender, test->remote_net_if_util[0], test->remote_net_if_util[1], test->remote_net_if_util[2], test->remote_net_if_util[3], test->remote_net_if_util[4]);
+		printf("DEBUG: Done with that");
 	    }
         }
 
